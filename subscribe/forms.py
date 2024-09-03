19d8b43c862d3e2e4c937c66435befd2abbcdf0e
Tony031201 <atuony0312@gmail.com>
2024-09-03 16:56:24 -0700

    update_html

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -4,4 +4,8 @@ from .models import subscribe
 class SubscribeTableForm(forms.ModelForm):
     class Meta:
         model = subscribe
-        fields ='__all__'
+        fields = ['name','email']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter email'}),
+        }
